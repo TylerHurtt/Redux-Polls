c52515fc7af4c21bd6d10bb85c627c20a6607a26
Sweet T <thurtt@rocketmail.com>
2020-06-07 00:11:52 -0700

    Added loadingBarReducer from react-redux-loading

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,11 @@ import { combineReducers } from 'redux';
 import authed from './authed';
 import users from './users';
 import polls from './polls';
+import { loadingBarReducer as loadingBar } from 'react-redux-loading';
 
-export default combineReducers({ authed, users, polls });
+export default combineReducers({
+  authed,
+  users,
+  polls,
+  loadingBar,
+});
