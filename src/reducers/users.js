f95eec6fe2aca2cb91c78c59771f7be42155ce66
Sweet T <thurtt@rocketmail.com>
2020-06-08 15:42:00 -0700

    Updated reducers for polls and users for adding new polls

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,9 +1,19 @@
 import { RECEIVE_USERS } from '../actions/users';
+import { ADD_POLL } from '../actions/polls';
 
 export default function users(state = {}, action) {
   switch (action.type) {
     case RECEIVE_USERS:
       return { ...state, ...action.users };
+    case ADD_POLL:
+      const { author, id } = action.poll;
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          polls: state[author].polls.concat[id],
+        },
+      };
     default:
       return state;
   }
