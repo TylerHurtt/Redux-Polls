31bb748208da2f6f865d04e1391f3934d94fbbad
Sweet T <thurtt@rocketmail.com>
2020-06-11 22:37:06 -0700

    Added ADD_ANSWER action to users reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,17 +1,31 @@
 import { RECEIVE_USERS } from '../actions/users';
 import { ADD_POLL } from '../actions/polls';
+import { ADD_ANSWER } from '../actions/answers';
 
 export default function users(state = {}, action) {
   switch (action.type) {
     case RECEIVE_USERS:
       return { ...state, ...action.users };
     case ADD_POLL:
-      const { author, id } = action.poll;
+      const { author, id } = this.props;
       return {
         ...state,
         [author]: {
           ...state[author],
-          polls: state[author].polls.concat[id],
+          polls: state[author].polls.concat([id]),
+        },
+      };
+    case ADD_ANSWER:
+      const { authed } = action;
+      const pid = action.id;
+
+      const user = state[authed];
+
+      return {
+        ...state,
+        [authed]: {
+          ...user,
+          answers: user.answers.concat([pid]),
         },
       };
     default:
